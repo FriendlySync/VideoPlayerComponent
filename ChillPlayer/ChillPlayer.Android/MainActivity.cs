a09c92e7b462232231e9eadf452d18de75c32cb5
George Tatar <george@example.com>
2022-03-10 09:27:57 -0500

    Last commit from adamfisher/Xamarin.Forms.VideoPlayer repo.

---
+++ ChillPlayer/ChillPlayer.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Octane.Xamarin.Forms.VideoPlayer;
+using Octane.Xamarin.Forms.VideoPlayer.Android;
+using Xamarin.Forms;
+
+namespace ChillPlayer.Droid
+{
+    [Activity(Label = "Octane.Xamarin.Forms.VideoPlayer", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Forms.Init(this, bundle);
+            FormsVideoPlayer.Init();
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ ChillPlayer/ChillPlayer/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ChillPlayer.Pages;
+using Xamarin.Forms;
+
+namespace ChillPlayer
+{
+    public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+		    MainPage = new NavigationPage(new MainMenuPage());
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
